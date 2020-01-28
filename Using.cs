84c59de039d559530d7e5f94bf8a1d66d05ce38c
nikitha31 <59816775+nikitha31@users.noreply.github.com>
2020-01-28 14:52:39 +0530

    Add files via upload

---
+++ Using.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingDemo
+{
+    class UsingDemo
+    {
+
+        static void Main(string[] args)
+        {
+            var font2 = new Font("Arial", 10.0f);
+            using (font2)
+            {
+                font2.GetHeight();
+            }
+            font2.GetHeight();
+        }
+        
+
+    }
+
+}
+
